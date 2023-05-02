849094768ac8b4c173487fffc825eaba1aaac592
Kevin <105953620+K7NGhost@users.noreply.github.com>
2023-05-01 21:36:27 -0400

    Added title and game logic

--- script.js
+++ script.js
@@ -1,17 +1,72 @@
 let playerCounter = 0;
 let computerCounter = 0;
 
+const rockBtn = document.querySelector('#btnR');
+const paperBtn = document.querySelector('#btnP');
+const scissorBtn = document.querySelector('#btnS');
+
+const rockImg = document.getElementById('rockImg');
+const paperImg = document.getElementById('paperImg');
+const scissorImg = document.getElementById('scissorImg');
+
+const rockImg2 = document.getElementById('rockImg2');
+const paperImg2 = document.getElementById('paperImg2');
+const scissorImg2 = document.getElementById('scissorImg2');
+
+
+rockBtn.addEventListener('click', () => {
+    playRock();
+})
+
+function playRock() {
+    rockImg.classList.add('active');
+    const computerChoice = getComputerChoice();
+    showImg(computerChoice);
+    game('rock', computerChoice);
+}
+
+function showImg(computerSelection) {
+    if(computerSelection === 'rock') {
+        rockImg2.classList.add('active');
+        paperImg2.classList.remove('active');
+        scissorImg2.classList.remove('active');
+        return;
+    }
+    if(computerSelection === 'paper') {
+        paperImg2.classList.add('active');
+        rockImg2.classList.remove('active');
+        scissorImg2.classList.remove('active');
+        return;
+    }
+    if(computerSelection === 'scissors') {
+        scissorImg2.classList.add('active');
+        paperImg2.classList.remove('active');
+        rockImg2.classList.remove('active');
+        return
+    }
+    rockImg2.classList.remove('active');
+    paperImg2.classList.remove('active');
+    scissorImg2.classList.remove('active');
+}
+
+function restartImg() {
+    rockImg.classList.remove('hidden');
+    rockImg2.classList.remove('hidden');
+    paperImg2.classList.remove('hidden');
+    scissorImg2.classList.remove('hidden');
+}
+
 function getComputerChoice() {
     const outcome = ['rock', 'paper', 'scissors']
     const aOutcome = outcome[Math.floor(Math.random()*3)];
     return aOutcome;
 }
 
-// function playerSelection() {
-//     return prompt("Enter rock, paper, or scissors").toLowerCase();
-// }
+function playerSelection(selection) {
+    return selection
+}
 
-function playRound(computerChoice, playerChoice) {
+function playRound(playerChoice, computerChoice) {
     if(computerChoice === 'rock' & playerChoice === 'paper') {
         playerChoice++;
         return 'You win! Paper beats rock';
@@ -48,14 +103,10 @@ function playRound(computerChoice, playerChoice) {
     
 }
 
-// function game() {
-//     for(let i = 0; i < 5; i++) {
-//         const playerSelect = playerSelection();
-//         const computerSelect = getComputerChoice();
-//         console.log(playRound(playerSelect, computerSelect));
-//         console.log("Player score = " + playerCounter);
-//         console.log("Computer score = " + computerCounter);
-//     }
-// }
-
-// game();
+function game(playerSelection, computerSelection) {
+    const playerSelect = playerSelection;
+    const computerSelect = computerSelection;
+    console.log(playRound(playerSelect, computerSelect));
+    console.log("Player score = " + playerCounter);
+    console.log("Computer score = " + computerCounter);
+}
